57d051e22dd535becd0ed914c3a36ff919666a65
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-15 22:52:17 +0200

    Update index.php
    Déclaration d'une premier fonction "additionner" 2 nombres

--- 6 - Utiliser les fonctions/index.php
+++ 6 - Utiliser les fonctions/index.php
@@ -6,6 +6,12 @@
     <title>6 - Utiliser les fonctions</title>
 </head>
 <body>
-    
+    <?php
+        function additionner($nb1, $nb2){
+            echo $nb1 + $nb2;
+        }
+
+    ?>
+    <p><?php echo additionner(12, 52); ?></p>  
 </body>
 </html>
