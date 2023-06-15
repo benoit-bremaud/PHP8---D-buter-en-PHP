da56e8235db35207ae578199b85b637b0e88eb12
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-15 20:48:52 +0200

    Update index.php
    Affichage d'un texte en utilisant une variable (affichage indirect)

--- Les Bases/index.php
+++ Les Bases/index.php
@@ -12,7 +12,18 @@
     /* commentaire
     sur plusieurs
     lignes */
-        echo "<h1>Hello World !!</h1>"; # permet d'afficher du texte en code php attention on finit toujours par ";"
-    ?> <!-- balise de fermeture du code php-->
+        echo "<h1>Hello World !!</h1>";
+        // introduction aux variables
+        // on décalre une variable en PHP en commencant par "$"
+        // ici nous sommes en convention camel case
+        $nomPersonne = "Benoit";
+
+        echo $nomPersonne;
+
+        echo "<h1>Bonjour $nomPersonne</h1>";
+        
+        echo "<p>Bonjour Benoit</p>";
+
+    ?>
 </body>
 </html>
