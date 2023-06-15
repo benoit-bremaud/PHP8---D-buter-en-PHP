8620b13b6feb26fb0ff67ea0556c9eec754fbb5d
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-15 20:39:15 +0200

    Update index.php
    Syntaxe des commentaires en PHP vs HTML

--- Les Bases/index.php
+++ Les Bases/index.php
@@ -6,7 +6,12 @@
     <title>Document</title>
 </head>
 <body>
+    <!-- commentaire HTML -->
     <?php # balise d'ouverture du code php
+    // Commentaire simple ligne
+    /* commentaire
+    sur plusieurs
+    lignes */
         echo "<h1>Hello World !!</h1>"; # permet d'afficher du texte en code php attention on finit toujours par ";"
     ?> <!-- balise de fermeture du code php-->
 </body>
