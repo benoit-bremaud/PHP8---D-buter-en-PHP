e093984fa5437f71d1506d8cdfdb2b571a6037ba
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-15 22:56:11 +0200

    Update index.php
    Deuxième fonction simple

--- 6 - Utiliser les fonctions/index.php
+++ 6 - Utiliser les fonctions/index.php
@@ -10,8 +10,17 @@
         function additionner($nb1, $nb2){
             echo $nb1 + $nb2;
         }
+        function saluer($salutation, $prenom, $nom){
+            echo "$salutation $prenom $nom";
+        }
 
     ?>
-    <p><?php echo additionner(12, 52); ?></p>  
+    <p><?php  additionner(12, 52); ?></p>
+    <p><?php  additionner(26, 85); ?></p>  
+    <p><?php  additionner(15, 2); ?></p>  
+    <p><?php  additionner(30, 25); ?></p> 
+    
+    <h1><?php saluer("Salut", "Benoit", "Bremaud") ?></h1>
+
 </body>
 </html>
