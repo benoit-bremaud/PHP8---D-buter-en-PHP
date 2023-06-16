9bdfbe70492c99931f8c9f8ec3d78f211cadce44
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 17:05:30 +0200

    Update articles.php
    Inclusion de :
    - (require_once) de la fonction 'connect.php' dans le dossier 'includes'
    - la section 'header.php'
    - la section 'navbar.php'
    - la section 'footer.php' en pied de page
    On définit la requête qui va aller chercher les informations des articles dans la base 'articles'
    On exécute le requête puis on récupère les données avec fletchAll
    On affiche la liste des articles
    A l'aide d'une boucle (foreach) on affiche les informations de chaque article récupérées dans $articles
    Ici, au lieu d'écrire des { } pour la fonction 'foreach()', on place " : " à la place de la première " { " puis en fin de boucle, on utilise la fonction 'endforeach;'

--- 12 - Afficher donnees base MySQL/articles.php
+++ 12 - Afficher donnees base MySQL/articles.php
@@ -1,18 +1,43 @@
 <?php
-    // On définit le titre de la page
-    $titre = "Liste des articles";
+// On va chercher les articles dans la base
+// On se connecte à la base
+require_once "includes/connect.php";
 
-    // On inclue le header
-    include "includes/header.php";
+// On écrit la requête
+$sql = "SELECT * FROM `articles` ORDER BY `created_at` DESC";
 
-    // On inclue la navbar
-    include "includes/navbar.php";
+// On exécute à la requête
+// Ici requête non préparée
+$requete = $db->query($sql);
+
+// On récupère les données
+$articles = $requete->fetchAll();
+
+// On définit le titre de la page
+$titre = "Liste des articles";
+
+// On inclue le header
+include "includes/header.php";
+
+// On inclue la navbar
+include "includes/navbar.php";
 
     
-    // On écrit le contenu de la page
+// On écrit le contenu de la page
 ?>
 
-<p>Contenu de la page articles</p>
+<h1>Liste des articles</h1>
+
+<!-- Après un fetchAll il y a forcément une boucle -->
+<section>
+<?php foreach($articles as $article): ?>
+    <article>
+        <h1><?= $article["title"] ?></h1> <!--?= ça fait un "echo"-->
+        <p>Publié le <?= $article["created_at"] ?></p>
+        <div><?= $article["content"] ?></div>
+    </article>    
+<?php endforeach; ?>
+</section>
 
 <?php
 
