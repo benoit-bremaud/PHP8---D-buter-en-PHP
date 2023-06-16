6f6e9bdad8b0f75251eb6dbb76e6b0b0b214896b
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 15:22:57 +0200

    Create articles.php
    Création du fichier 'articles.php' dans '11 - Requetes preparees - PDO'
    Inclusion des fichiers :
    - header.php
    - navbar.php
    - footer.php
    Définition du titre de la page avec la variable $titre, variable récupéré après par 'header.php'
    Ce fichier n'a pas de fonction particulière pour le moment

---
+++ 11 - Requetes preparees - PDO/articles.php
@@ -0,0 +1,22 @@
+<?php
+    // On définit le titre de la page
+    $titre = "Liste des articles";
+
+    // On inclue le header
+    include "includes/header.php";
+
+    // On inclue la navbar
+    include "includes/navbar.php";
+
+    
+    // On écrit le contenu de la page
+?>
+
+<p>Contenu de la page articles</p>
+
+<?php
+
+    // On inclue le footer
+    include "includes/footer.php";
+
+?>
