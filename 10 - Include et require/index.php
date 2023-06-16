6220d2ab96b5ed1a22f577029e859cf9b8ce178d
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 13:34:13 +0200

    Create index.php
    Création du fichier index.php
    Définition d'un fichier php type avec :
    navbarre
    coprs de texte
    footer

---
+++ 10 - Include et require/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INCLUDE et REQUIRE</title>
+</head>
+<body>
+    <nav>
+        <h1>Titre</h1>
+        <ul>
+            <li>Accueil</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    <?php
+    // Diviser le code
+
+    ?>
+    <footer>
+        <p>&copy: 2023</p>
+    </footer>
+
+    
+</body>
+</html>
