3aa4361b837fedf9a4440a3c7a0f485c1e97a427
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 13:12:37 +0200

    Create index.php
    Définition et paramétrage des constantes d'environnement
    Définition et paramétrage du Data Source Name (DSN)
    Connection à la base de données avec la fonction try / catch
    Définition du mode fetch sur "tableau associatif" par défaut
    Définition et initialisation de la requéte sql -> lecture des données
    Récupération des données par la fonction fetchAll dans la variable $user
    Affichage du résultat de la requéte par la fonction var_dump.

---
+++ 9 - PDO et CRUD/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO et CRUD</title>
+</head>
+<body>
+    <?php
+        // Constantes d'environement
+        define("DBHOST", "localhost");
+        define("DBUSER", "root");
+        define("DBPASS", "motdepasse1@");
+        define("DBNAME", "tuto_php");
+
+        // Avec PDO nous avons besoin d'un DSN (Data Source Name)
+        // $dsn = "mysql:dbname=tuto_php;host=localhost";
+        $dsn = "mysql:dbname=".DBNAME.";host=".DBHOST;
+
+        // On va se connecter à la base
+        // On utilise le "try catch"
+        try {
+            // On va instancier PDO, créer une instance, un élément de PDO qu'on va mettre dans une variable
+            $db = new PDO($dsn, DBUSER, DBPASS);
+
+            echo "On est connecté";
+
+            // On s'assure d'envoyer les données en UTF8 (pour la gestion des accents)
+            $db->exec("SET NAMES utf8");
+
+            // On peut définir le mode de "fetch" par défaut
+            $db->setAttribute(  PDO::ATTR_DEFAULT_FETCH_MODE, # Définition du mode fetch par défaut
+                                PDO::FETCH_ASSOC); # Définition du mode par défaut en tableau associatif
+
+        } catch (PDOException $e) {
+            die("Erreur:".$e->getMessage());
+        }
+
+        // Ici nous sommes connectés à la base de données
+        // On peut dès lors récupérer la liste des utilisateurs (users)
+
+        $sql = "SELECT * FROM `users`";
+
+        // On exécute directement la requête
+
+        $requete = $db->query($sql);
+
+        var_dump($requete);
+
+        // On récupère les données (fetch ou fetchall)
+        $user = $requete->fetch(); # Définition du "fetch" déjà faite plus haut , définit en tableau associatif
+
+        $user = $requete->fetchAll();
+
+        echo "<pre>";
+        var_dump($user);
+        echo "</pre>";
+
+    ?>    
+</body>
+</html>
