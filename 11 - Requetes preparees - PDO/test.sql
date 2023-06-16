9d900661337ac39d5de5055c3971c9d720312798
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 15:58:27 +0200

    Update test.sql
    Exemple avec un autre type d'injection

--- 11 - Requetes preparees - PDO/test.sql
+++ 11 - Requetes preparees - PDO/test.sql
@@ -10,3 +10,15 @@ SELECT * FROM `users` WHERE `username` = 'admin'; --' AND `pass` = '$password'
 SELECT * FROM `users` WHERE `username` = 'admin'; -- AND `pass` = '$password'
 
 -- Du coup l'intégralité des informations de l'utilisateur 'admin' se voient affichées à l'écran !!
+
+-- Autre type d'injection sql
+-- Requete de base
+SELECT * FROM `users` WHERE `username` = '$username' AND `pass` = '$password';
+
+-- Requete après injection
+SELECT * FROM `users` WHERE `username` = 'admin' AND `pass` = 'inconnu' OR 1=1; --';
+
+-- C'est comme ci on écrivait
+SELECT * FROM `users` WHERE 1=1;
+
+-- Avec ce type injection, on peut récupérer du coup l'intégralité de la bdd
