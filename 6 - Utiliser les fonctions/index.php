d243b2a49bdebb83df7fe079c72f4060a28f53c2
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 10:41:21 +0200

    Update index.php
    Fonction avec paramètres optionnels et/ou facultatifs
    Utilisation de la fonction "return" dans une fonction et récupération du résultat suite à l'appel de cette fonction.
    Utilisation du "php doc" permet de définir une petite documentation pour la fonction créée.

--- 6 - Utiliser les fonctions/index.php
+++ 6 - Utiliser les fonctions/index.php
@@ -7,11 +7,33 @@
 </head>
 <body>
     <?php
+        // fonction qui affiche directement le résultat avec la fonction "echo"
         function additionner($nb1, $nb2){
             echo $nb1 + $nb2;
         }
-        function saluer($prenom, $nom, $salutation = "Hello"){
-            echo "$salutation $prenom $nom";
+        // Fonction avec paramètres optionnels à la fin
+        /**
+         * Fonction qui permet d'afficher un texte de salutation
+         *
+         * @param string $prenom
+         * @param string $nom
+         * @param string $civilite
+         * @param string $salutation
+         * @return string $salutation $civilite $prenom $nom
+         */
+        function saluer($prenom, $nom, $civilite = "", $salutation = "Hello"){
+            echo "$salutation $civilite $prenom $nom";
+        }
+        // Fonction avec une fonction return
+        /**
+         * Permet de calculer la différence entre 2 valeurs
+         *
+         * @param float $nb1
+         * @param float $nb2
+         * @return float différence des 2 nombres
+         */
+        function soustraction($nb1, $nb2){
+            return $nb1 - $nb2;
         }
 
     ?>
@@ -22,7 +44,18 @@
     
     <h1><?php saluer("Salut", "Benoit", "Bremaud") ?></h1>
 
+    <!-- Depuis PHP8 on peut utiliser des arguments nommés -->
     <h1><?php saluer(prenom: "Benoit", nom: "Bremaud") ?></h1>
+
+    <!-- plus pratique avec les champs nommés -->
+    <h1><?php saluer(prenom: "Benoit", nom: "Bremaud", civilite: "Mr.", salutation: "Bonjour") ?></h1>
+
+    <!-- récupérer l'information de la fonction "soustraction" avec fonction "return" -->
+    <?php
+        $total = soustraction(18, 8);
+        var_dump($total);
+        echo $total;
+    ?>
 
 
 </body>
