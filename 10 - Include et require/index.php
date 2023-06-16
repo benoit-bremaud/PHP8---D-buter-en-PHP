b1cabbcc6b08c156896540c92d09fffba5fcb8ab
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-16 13:40:11 +0200

    Update index.php
    Suppression du "header" -> couper/coller dans le fichier header.php
    Suppression du "navbar" -> couper/coller dans le fichier navbar.php
    Suppression du "footer" -> couper/coller dans le fichier footer.php
    
    Pour ne garder que le corps de la page index.php

--- 10 - Include et require/index.php
+++ 10 - Include et require/index.php
@@ -1,26 +1,26 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>INCLUDE et REQUIRE</title>
 </head>
-<body>
-    <nav>
+<body> -->
+    <!-- <nav>
         <h1>Titre</h1>
         <ul>
             <li>Accueil</li>
             <li>Contact</li>
         </ul>
-    </nav>
+    </nav> -->
     <?php
     // Diviser le code
 
     ?>
-    <footer>
+    <!-- <footer>
         <p>&copy: 2023</p>
     </footer>
 
     
 </body>
-</html>
+</html> -->
