b1742a01c7584d5e73717558e5bb3c2a0c95e397
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-17 19:03:49 +0200

    Create article.php
    Création de la page article.php qui permet d'afficher l'article sélectionné dans la page articles.php.
    On teste si l'id existe, sinon on renvoi sur la page articles.php
    On récupère ensuite l'id
    On se connecte à la base
    On écrit la requete
    On prépare la requete
    On injecte la requete
    On injecte les paramètres
    On éxecute la requete
    On récupère l'article
    On vérifie si l'article est vide
    Si pas d'article afficher message "Article inexistant"
    Si on a un article :
    On définit le titre de la page en récupérant le titre de l'article de la base
    On inclue le header
    On inclue la navbar
    On écrit le contenu de la page qui sont les informations du l'article
    On inclue enfin le footer

---
+++ 12 - Afficher donnees base MySQL/article.php
@@ -0,0 +1,61 @@
+<?php
+// On vérifie si on a un id
+if (!isset($_GET["id"]) || empty($_GET["id"])){
+    // Je n'ai pas d'id
+    header("Location: articles.php");
+    exit;
+}
+
+// Je récupère l'id
+$id = $_GET["id"];
+
+// On se connecte à la base
+require_once "includes/connect.php";
+
+// On va chercher l'article dans la base
+// On écrit la requête
+$sql = "SELECT * FROM `articles` WHERE `id` = :id";
+
+// On prépare la requête
+$requete = $db->prepare($sql);
+
+// On injecte les paramètres
+$requete->bindValue(":id", $id, PDO::PARAM_INT); # on évite les injections de script en convertissant en un entier
+
+// On exécute la requête
+$requete->execute();
+
+// On récupère l'article
+$article = $requete->fetch();
+
+// On vérifie si l'article est vide
+if (!$article) {
+    // Pas d'article, erreur 404
+    http_response_code(404);
+    echo "Article inexistant";
+    exit;
+}
+// Ici on a un article
+
+// On définit le titre de la page
+$titre = strip_tags($article["title"]);
+
+// On inclue le header
+include "includes/header.php";
+
+// On inclue la navbar
+include "includes/navbar.php";
+    
+// On écrit le contenu de la page
+?>
+<article>
+    <h1><?= strip_tags($article["title"]) ?></h1> <!--?= ça fait un "echo"-->
+    <p>Publié le <?= $article["created_at"] ?></p>
+    <div><?= strip_tags($article["content"]) ?></div> <!-- strip_tags() permet de protèger les données contre des failles de codage-->
+</article>    
+<?php
+
+    // On inclue le footer
+    include "includes/footer.php";
+
+?>
