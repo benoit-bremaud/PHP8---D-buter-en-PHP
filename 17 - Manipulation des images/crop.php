085f58c6d85d46b51c26d7bb9e81668836c91512
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-06-24 15:45:01 +0200

    feat(crop): Ajout fonction qui recadre (découpe) une image

--- 17 - Manipulation des images/crop.php
+++ 17 - Manipulation des images/crop.php
@@ -11,3 +11,43 @@ $image = __DIR__ . "/uploads/" . $fichier;
 // On récupère les infos de l'image
 // ATTENTION, normalement il y a des contrôles à faire avant
 $infos = getimagesize($image);
+
+// J'ouvre mon image
+switch ($infos["mime"]) {
+    case 'image/png':
+        // On ouvre l'image png
+        $imageSource = imagecreatefrompng($image);
+        break;
+    case 'image/jpeg':
+        // On ouvre l'image jpeg
+        $imageSource = imagecreatefromjpeg($image);
+        break;    
+    default:
+        die("Fomat d'image incorrect");
+}
+
+// On recadre l'image selon les coordonnées suivants
+$nouvelleImage = imagecrop($imageSource, [
+    "x" => 200, // Se placer à 200px du bord gauche
+    "y" => 200, // Descendre de 200 px
+    "width" => 300, // Trace un rectange de 300 de largeur et, 
+    "height" => 150 // 150 de haut
+]);
+
+// On enregistre la nouvelle image sur le serveur
+switch ($infos["mime"]) {
+    case 'image/png':
+        // On enregistre l'image
+        imagepng($nouvelleImage, __DIR__ . "/uploads/crop-" . $fichier);
+        break;
+    case 'image/jpeg':
+        // On enregistre l'image
+        imagejpeg($nouvelleImage, __DIR__ . "/uploads/crop-" . $fichier);
+        break;
+
+}
+
+// On détruit les images dans la mémoire
+imagedestroy($nouvelleImage);
+
+
